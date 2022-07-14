64c0db7ed8c36eb7bf8aac4046d5a148e7f23d2d
Abdulmateentechibits <mateen@techibits.com>
2022-07-14 13:06:19 -0700

    Starter app

---
+++ constants/index.js
@@ -0,0 +1,47 @@
+import constants from "./constants";
+import theme, { COLORS, SIZES, FONTS, lightTheme, darkTheme } from "./theme";
+import images from "./images";
+import icons from "./icons";
+import {banners,
+    flashDeals,
+    promoItems,
+    categories,
+    quickALinks,
+    services,
+    chartData,
+    orders,
+    flashSales,
+    brands,
+    flashSaleItems,
+    featuredProducts,
+    topSearch,
+    suggestedSearch,
+    recentSearch
+} from "./dummyData"
+
+export {
+    constants,
+    theme,
+    COLORS,
+    SIZES,
+    FONTS,
+    images,
+    icons,
+    banners,
+    flashDeals,
+    promoItems,
+    categories,
+    quickALinks,
+    services,
+    chartData,
+    orders,
+    flashSales,
+    brands,
+    flashSaleItems,
+    featuredProducts,
+    topSearch,
+    suggestedSearch,
+    recentSearch,
+    lightTheme,
+    darkTheme,
+}
