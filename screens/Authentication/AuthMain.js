46d0e8daf4d5722228fc1bd5a2176c308c6d64d1
Abdulmateentechibits <mateen@techibits.com>
2022-07-20 13:35:41 -0700

    login component added

--- screens/Authentication/AuthMain.js
+++ screens/Authentication/AuthMain.js
@@ -12,21 +12,43 @@ import React, { useState } from "react";
 import { TextButtons } from "../../components";
 import { icons, images, FONTS, SIZES, COLORS } from "../../constants";
 import { Colors } from "react-native/Libraries/NewAppScreen";
+import { MotiView, useAnimationState } from "moti";
+import { Shadow } from "react-native-shadow-2";
 
 const AuthMain = () => {
-  const [mode, setMode] = useState('signin');
+  const [mode, setMode] = useState("signIn");
 
-  function renderSignIn(){
+  // Animation State
+  const animationState = useAnimationState({
+    signIn: {
+      height: SIZES.height * 0.5,
+    },
+    signUp: {
+      height: SIZES.height * 0.7,
+    },
+  });
 
-    
+  function renderSignIn() {
+    return (
+      <MotiView
+        state={animationState}
+        style={{
+          marginTop: SIZES.padding,
+          height: SIZES.height * 0.55,
+        }}
+      >
+        <Shadow>
+          <View style={styles.authContainer}>
+            <Text>Login</Text>
+          </View>
+        </Shadow>
+      </MotiView>
+    );
   }
-  function renderSignUp(){
-    
-    
-  }
+  function renderSignUp() {}
 
   function renderContainer() {
-    if (mode === "signIn") {
+    if (mode == "signIn") {
       return renderSignIn();
     } else {
       return renderSignUp();
@@ -59,4 +81,13 @@ const AuthMain = () => {
 
 export default AuthMain;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  authContainer: {
+    flex: 1,
+    width: SIZES.width - SIZES.padding * 2,
+    paddingHorizontal: SIZES.padding,
+    paddingVertical: SIZES.radius,
+    borderRadius: SIZES.radius,
+    backgroundColor: COLORS.light,
+  },
+});
